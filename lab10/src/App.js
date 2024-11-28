0a036f5b94c282b15f7db625fcaaaba73b131d31
Nima <105126750+NimaHagh@users.noreply.github.com>
2024-11-28 17:38:15 +0000

    Add React project for lab10 to Webdesign530 repository

---
+++ lab10/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nima Hagh Doust - Lab 10 - React</h1>
+        <section>
+          <p>
+            To do problem 3 of Lab10, I installed Node.js and npm. Using npm, I ran npx create-react-app my-react-app to start the project, and it will install all the required libraries and tools needed to run a simple react application.
+            After navigating to the project folder, I ran <code>npm start</code> to launch the development server.
+          </p>
+        </section>
+        <section>
+          <p>
+            I modified the App.js code to create this page using simple js and html. I learned that React components use JSX, which is a syntax extension that allows us to write HTML within JavaScript.
+            I also used CSS that was already there and no css was changed.
+          </p>
+        </section>
+        <section>
+          <p>
+            One thing I struggled with were the commands to use to start a project in React, but after watvhing some youtube videos and reading the official React documentation
+            and following online tutorials to get a better understanding.
+          </p>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
